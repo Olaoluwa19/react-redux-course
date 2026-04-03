5084dfde09064be1f237985679bd5648bfb79203
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-04-03 02:52:51 +0100

    feat: added componemt to App files

--- 02_lesson_data_flow/src/App.jsx
+++ 02_lesson_data_flow/src/App.jsx
@@ -1,7 +1,13 @@
-import Counter from "./features/counter/counter";
+import PostsList from "./features/post/PostsList";
+import AddPostForm from "./features/post/AddPostForm";
 
 function App() {
-  return <main className="App"></main>;
+  return (
+    <main className="App">
+      <AddPostForm />
+      <PostsList />
+    </main>
+  );
 }
 
 export default App;
