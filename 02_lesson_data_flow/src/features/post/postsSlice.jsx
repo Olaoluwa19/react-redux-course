bd436d451fdcf62238b1821913516ae1a080626a
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-04-03 03:51:55 +0100

    feat: added date and reaction to initialState amd reducer

--- 02_lesson_data_flow/src/features/post/postsSlice.jsx
+++ 02_lesson_data_flow/src/features/post/postsSlice.jsx
@@ -1,15 +1,32 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
 
 const initialState = [
   {
     id: 1,
     title: "Learning Redux Toolkit",
     content: "I have heard good things!",
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
   },
   {
     id: 2,
     title: "Slice...",
     content: "The more I say slice, the more I want pizza.",
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
   },
 ];
 
@@ -21,12 +38,14 @@ const postSlice = createSlice({
       reducer(state, action) {
         state.push(action.payload);
       },
-      prepare(title, content) {
+      prepare(title, content, userId) {
         return {
           payload: {
             id: nanoid(),
             title,
             content,
+            date: new Date().toISOString(),
+            userId,
           },
         };
       },
