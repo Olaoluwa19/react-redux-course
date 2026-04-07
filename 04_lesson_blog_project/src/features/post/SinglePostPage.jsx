ca92c5a264b5cfbf3093e45eb1ac1f13ada00422
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-04-07 06:09:20 +0100

    created a single page component to turn th e blog app into asingle page app

---
+++ 04_lesson_blog_project/src/features/post/SinglePostPage.jsx
@@ -0,0 +1,34 @@
+import { useSelector } from "react-redux";
+import { selectPostById } from "./postsSlice";
+
+import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
+import ReactionButtons from "./ReactionButtons";
+
+const SinglePostPage = () => {
+  // retrieve postId
+
+  const post = useSelector((state) => selectPostById(state, postsId));
+
+  if (!post) {
+    return (
+      <section>
+        <h1>Post not found !</h1>
+      </section>
+    );
+  }
+
+  return (
+    <article>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+      <p className="postCredit">
+        <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
+      </p>
+      <ReactionButtons pst={post} />
+    </article>
+  );
+};
+
+export default SinglePostPage;
