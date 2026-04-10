188df8db5fbb6974b788531ea6f0fc6a7ef8943f
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2026-04-10 07:09:05 +0100

    feat: added fetcchPosts to main.js to allow posts to load at Load time

--- 04_lesson_blog_project/src/main.jsx
+++ 04_lesson_blog_project/src/main.jsx
@@ -4,9 +4,11 @@ import "./index.css";
 import App from "./App.jsx";
 import { store } from "./app/store.jsx";
 import { Provider } from "react-redux";
+import { fetchPosts } from "./features/post/postsSlice.jsx";
 import { fetchUsers } from "./features/users/usersSlice.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+store.dispatch(fetchPosts());
 store.dispatch(fetchUsers());
 
 createRoot(document.getElementById("root")).render(
